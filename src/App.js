7ce312bef8bae5cc9f4fa10f38c29a4eef39c7ed
tullioColacicco <56273725+tullioColacicco@users.noreply.github.com>
2024-12-04 17:45:38 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div class="container">
+      <div className="header">
+        <h1 className="one">Yankess</h1>
+        <h1 className="two">news</h1>
+        <h1 className="three">Juan Soto</h1>
+      </div>
+      <div className="content">
+        <h1>hello world</h1>
+      </div>
+
+      <div className="footer"></div>
+    </div>
+  );
+}
+
+export default App;
